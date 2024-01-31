fb7c529dd029a2a71c7bb368b26cf389af20d8c1
A. Yilmaz <79148268+yilmazabdurrah@users.noreply.github.com>
2024-01-31 16:22:16 +0000

    semantic description publish enabled

--- franka_moveit_config/launch/moveit.launch.py
+++ franka_moveit_config/launch/moveit.launch.py
@@ -122,6 +122,7 @@ def generate_launch_description():
         'publish_geometry_updates': True,
         'publish_state_updates': True,
         'publish_transforms_updates': True,
+        'publish_robot_description_semantic': True,
     }
 
     # Start the actual move_group node/action server
